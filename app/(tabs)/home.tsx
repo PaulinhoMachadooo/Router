9e14b7ed1a7ffe812d9bdcccbf06ebbccc568315
PaulinhoMachadooo <paulinhomachadoo20@gmail.com>
2024-10-08 22:18:15 -0300

    exportando

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,29 +1,20 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import { getAllCategories } from "../../services/category";
 import { CategoryItem } from "../../components/category-item";
 
 export default function Screen() {
-    const categories = getAllCategories();
+  const categories = getAllCategories();
 
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={categories}
-                renderItem={({ item }) => <CategoryItem data={item} />}
-                keyExtractor={item => item.id.toString()}
-                style={styles.list}
-            />
-        </View>
-    );
+  return (
+    <View>
+      <Image
+        source={require("../../assets/guia+logo.png")}
+        style={{
+          width: 100,
+          height: 40,
+        }}
+      />
+    </View>
+    
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    list: {
-        flex: 1,
-        width: '100%',
-        padding: 20
-    }
-});
