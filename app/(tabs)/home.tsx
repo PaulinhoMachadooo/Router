772ebefd89b18310ed23f3c039b29bb102ddbbe2
PaulinhoMachadooo <paulinhomachadoo20@gmail.com>
2024-10-10 13:40:56 -0300

    Logo

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,18 +1,53 @@
-import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import AppCarousel from "../../components/AppCarousel";
 import Parallax from "../../components/Parallax";
+import { MagnifyingGlassIcon, Bars3Icon } from "react-native-heroicons/outline";
+import { StatusBar } from "expo-status-bar";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+let width = Dimensions.get("window").width;
 
 export default function Screen() {
   return (
-    <View>
-      <View style={styles.logo}>
-        <Image source={require("../../assets/icon2.png")} />
-      </View>
-      <View style={styles.container}>
-        {/* <AppCarousel /> */}
-        <Parallax />
-      </View>
-    </View>
+    <SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={true}
+        contentContainerStyle={{
+          paddingBottom: 50,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            
+          }}
+        >
+          <Bars3Icon size={hp(5)} color={"gray"} />
+          <Image
+            source={require("../../assets/icon2.png")}
+            style={{
+              width: hp(20),
+              height: hp(8),
+            }}
+          />
+        </View>
+
+        <View style={styles.container}>
+          <Parallax />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -22,10 +57,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  logo: {
-    flex: 1,
-    width: 20,
-    height: 10,
   },
 });
